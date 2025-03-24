e3beab87f10fd97a7b36341975c6be891896c353
shrestha-bishal <bishal.shrestha@outlook.com.au>
2025-03-24 11:03:08 +1000

    - Added PasswordValidator

---
+++ validators/PasswordValidator.php
@@ -0,0 +1,61 @@
+<?php 
+namespace ValidatePHPCore\Validators;
+
+use ValidatePhpCore\ValidationError;
+use ValidatePHPCore\ValidatorBase;
+
+class PasswordValidator extends ValidatorBase 
+{
+    /**
+     * Constructor for the PhoneNumberValidation class.
+     *
+     * @param string $name The name of the validation.
+     * @param string $fieldName The name of the field to be validated.
+     * @param int $maxLength The maximum length of the number.
+     * @param int $minLength The minimum length of the number.
+     * @param bool $isRequired Whether the field is required or not. Default is true.
+     */
+    public function __construct(
+        string $name, 
+        string $fieldName, 
+        int $maxLength = 30,
+        int $minLength = 8,
+        bool $isRequired = true)
+    {
+        parent::__construct(
+            $name, 
+            $fieldName, 
+            $minLength,
+            $maxLength,
+            "password",
+            $isRequired
+        );
+    }
+
+    public function validate($fieldValue) : ?ValidationError
+    {
+        $error = null;
+
+        if (!preg_match('/[A-Z]/', $fieldValue)) {
+            return new ValidationError($this, "$this->name must contain at least one uppercase letter.");
+        }
+
+        if (!preg_match('/[a-z]/', $fieldValue)) {
+            return new ValidationError($this, "$this->name must contain at least one lowercase letter.");
+        }
+
+        if (!preg_match('/\d/', $fieldValue)) {
+            return new ValidationError($this, "$this->name must contain at least one number.");
+        }
+
+        if (!preg_match('/[\W_]/', $fieldValue)) {
+            return new ValidationError($this, "$this->name must contain at least one special character.");
+        }
+
+        // if (preg_match('/\s/', $fieldValue)) {
+        //     return new ValidationError($this, "$this->name cannot contain spaces.");
+        // }
+
+        return $error;
+    }
+}
