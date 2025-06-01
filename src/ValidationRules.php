375eb2111c2ff52ae3de6350fe9bf5478ad152a1
shrestha-bishal <bishal.shrestha@outlook.com.au>
2025-06-02 09:32:20 +1000

    - added ValidationRules to define the validation rules - added ValidationHandler to validate from the instance of any class - added example (initial)

---
+++ src/ValidationRules.php
@@ -0,0 +1,13 @@
+<?php
+namespace ValidatePhpCore;
+
+/**
+ * Class ValidationRules
+ *
+ * Provides static methods to define and retrieve validation rules
+ * for various objects or data structures.
+ */
+
+class ValidationRules {
+
+}
