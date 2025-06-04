0b831cabbaad9f089a528c015f71ec23d13b62df
shrestha-bishal <bishal.shrestha@outlook.com.au>
2025-06-04 14:00:08 +1000

    added support for nested object and array of objects

---
+++ src/validators/ObjectArrayValidator.php
@@ -0,0 +1,26 @@
+<?php 
+namespace PhpValidationCore\Validators;
+
+/**
+ * Class ObjectArrayValidator
+ *
+ * Validates a property that contains an array of objects, where each object is validated
+ * using the registered rules for its class (e.g. DTOs with ValidationRegistry).
+ *
+ * Usage:
+ * ```
+ * new ObjectArrayValidator('items', FreightItemDto::class)
+ * ```
+ *
+ * Requirements:
+ * - The target property must be an array.
+ * - Each element in the array must be an object of the specified type.
+ * - Validation rules must be registered for the given object class via ValidationRegistry.
+ *
+ * @package YourNamespace\Validators
+ */
+class ObjectArrayValidator {
+    public function __construct(
+        public string $propertyName,
+        public string $className) {}
+}
