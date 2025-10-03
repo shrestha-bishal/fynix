f9d90a678355ffc09b8f69d9d07c752303890b04
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2025-10-03 15:38:44 +1000

    removed the old examples from the source

--- examples/AppBootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use PhpValidationCore\Examples\Staff;
-use PhpValidationCore\Examples\ValidationRules;
-use PhpValidationCore\ValidationRegistry;
-
-ValidationRegistry::register(Staff::class, [ValidationRules::class, 'validateStaff']);
